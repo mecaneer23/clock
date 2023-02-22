ef1d4e03e4a18dffbbcbed471b6ef0f9c334052c
mecaneer23 <mecaneer23@gmail.com>
2023-02-22 16:44:18 -0600

    add digital clock

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ def analog_clock(hours, minutes):
     guard_inputs(hours, minutes)
     hours = to_twelve_hours(hours)
     minutes = int(round_to_nearest_minutes(minutes)) // 5
-    time = [combine_strings(h, m) for h, m in zip(time_to_ascii(hours, 1), time_to_ascii(minutes, 0))]
+    time = [combine_strings(h, m) for h, m in zip(time_to_analog(hours, 1), time_to_analog(minutes, 0))]
     return [
          r"        _____        ",
          r"     _.'_____'._     ",
@@ -24,7 +24,7 @@ def analog_clock(hours, minutes):
     ]
 
 
-def time_to_ascii(time, short=0):
+def time_to_analog(time, short=0):
     """short is a boolean value (0 or 1)"""
     return [
         [
@@ -234,6 +234,91 @@ def combine_strings(a, b):
 
 def digital_watch(hours, minutes):
     guard_inputs(hours, minutes)
+    minutes = str(minutes).rjust(2, "0")
+    hours = to_twelve_hours(hours)
+    a = time_to_digital(1)[1:] if len(str(hours)) == 2 else ["   ", "   "]
+    b = time_to_digital(int(str(hours)[-1]))
+    c = time_to_digital(int(minutes[0]))
+    d = time_to_digital(int(minutes[1]))
+    return [
+         r"       .--.-----.--.         ",
+         r"       |--|-----|--|         ",
+         r"       |--|     |--|         ",
+         r"       |  |-----|  |         ",
+         r"     __|--|     |--|__       ",
+         r"    /  |  |-----|  |  \      ",
+         r"   /   \__|-----|__/   \     ",
+         r"  /   ______---______   \/\  ",
+         r" /   /               \   \/  ",
+         fr"{{   /   {b[0]}   {c[0]} {d[0]} \   }}  ",
+         fr"|  {{{a[0]} {b[1]} . {c[1]} {d[1]}  }}  |-.",
+         fr"|  |{a[1]} {b[2]} . {c[2]} {d[2]}  |  | |",
+         r"|  {                   }  |-'",
+         r"{   \                 /   }  ",
+         r" \   `------___------'   /\  ",
+         r"  \     __|-----|__     /\/  ",
+         r"   \   /  |-----|  \   /     ",
+         r"    \  |--|     |--|  /      ",
+         r"     --|  |-----|  |--       ",
+         r"       |--|     |--|         ",
+         r"       |--|-----|--|         ",
+         r"       '--'-----'--'         ",
+    ]
+
+
+def time_to_digital(time):
+    return [
+        [
+            " _ ",
+            "| |",
+            "|_|",
+        ],
+        [
+            "   ",
+            "  |",
+            "  |",
+        ],
+        [
+            " _ ",
+            " _|",
+            "|_ ",
+        ],
+        [
+            " _ ",
+            " _|",
+            " _|",
+        ],
+        [
+            "   ",
+            "|_|",
+            "  |",
+        ],
+        [
+            " _ ",
+            "|_ ",
+            " _|",
+        ],
+        [
+            " _ ",
+            "|_ ",
+            "|_|",
+        ],
+        [
+            " _ ",
+            "  |",
+            "  |",
+        ],
+        [
+            " _ ",
+            "|_|",
+            "|_|",
+        ],
+        [
+            " _ ",
+            "|_|",
+            " _|",
+        ],
+    ][time]
 
 
 def guard_inputs(hours, minutes):
@@ -260,11 +345,19 @@ def print_ascii(ascii_array):
         print(row)
 
 
-def main():
+def main(args):
     from datetime import datetime
 
-    print_ascii(analog_clock(*[int(i) for i in datetime.now().strftime("%H %M").split()]))
-
+    time = [int(i) for i in datetime.now().strftime("%H %M").split()]
+    if args[0] == "-":
+        flags = args[1:]
+        if "c" in flags:
+            print_ascii(analog_clock(*time))
+        elif "d" in flags:
+            print_ascii(digital_watch(*time))
+    else:
+        print_ascii(analog_clock(*time))
 
 if __name__ == "__main__":
-    main()
+    import sys
+    main(sys.argv[1] if len(sys.argv) > 1 else " ")
