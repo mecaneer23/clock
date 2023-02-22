95efe4fefe062cc52cbd47c99000f61b476367f5
mecaneer23 <mecaneer23@gmail.com>
2023-02-22 10:55:47 -0600

    format and fix guard clauses

--- clock.py
+++ clock.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
 
+
 def analog_clock(hours, minutes):
-    if guard_inputs(hours, minutes):
+    if not guard_inputs(hours, minutes):
         return
+    hours = to_twelve_hours(hours)
     print(hours, minutes)
 
+
 def digital_watch(hours, minutes):
-    if guard_inputs(hours, minutes):
+    if not guard_inputs(hours, minutes):
         return
+
 
 def guard_inputs(hours, minutes):
     return 0 < hours <= 12 and 0 < minutes <= 60
 
+
+def to_twelve_hours(hours):
+    pass
+
+
 def main():
     from datetime import datetime
-    print(datetime.now())
+
     analog_clock(*[int(i) for i in datetime.now().strftime("%H %M").split()])
+
 
 if __name__ == "__main__":
     main()
