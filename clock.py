bbce421fa9a8104c2b2889668b05cc0aa823e78b
mecaneer23 <mecaneer23@gmail.com>
2024-01-23 17:39:10 -0600

    refactor: fix imports, remove md_table_to_lines

--- clock.py
+++ clock.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+"""A command line program to display the time in multiple formats."""
+# pylint: disable=missing-docstring
 
-from typing import List
+import os
+import sys
+from datetime import datetime
+
+from md_to_py import md_table_to_lines
 
 
 def analog_clock(hours: int, minutes: int) -> list[str]:
@@ -239,70 +245,6 @@ def combine_strings(a: str, b: str) -> str:
     return output
 
 
-def md_table_to_lines(
-    first_line_idx: int,
-    last_line_idx: int,
-    filename: str = "README.md",
-    remove: List[str] = [],
-) -> List[str]:
-    """
-    Converts a markdown table to a list of formatted strings.
-
-    Args:
-        first_line_idx (int): The index of the first line of the markdown table to be converted.
-        last_line_idx (int): The index of the last line of the markdown table to be converted.
-        filename (str, optional): The name of the markdown file containing the table. Default is "README.md".
-        remove (list[str], optional): The list of strings to be removed from each line. This is in the case of formatting that should exist in markdown but not python. Default is an empty list.
-
-    Returns:
-        list[str]: A list of formatted strings representing the converted markdown table.
-
-    Raises:
-        ValueError: If the last line index is less than or equal to the first line index.
-        FileNotFoundError: If the specified markdown file cannot be found.
-    """
-
-    # Check for valid line indices
-    if last_line_idx <= first_line_idx:
-        raise ValueError("Last line index must be greater than first line index.")
-
-    # Get raw lines from the markdown file
-    try:
-        with open(filename) as f:
-            lines = f.readlines()[first_line_idx - 1 : last_line_idx - 1]
-    except FileNotFoundError:
-        raise FileNotFoundError("Markdown file not found.")
-
-    # Remove unwanted characters and split each line into a list of values
-    for i, _ in enumerate(lines):
-        for item in remove:
-            lines[i] = lines[i].replace(item, "")
-        lines[i] = lines[i].split("|")[1:-1]
-    column_count = len(lines[0])
-    lines[1] = ["-" for _ in range(column_count)]
-
-    # Create lists of columns
-    columns = [[0, []] for _ in range(column_count)]
-    for i in range(column_count):
-        for line in lines:
-            columns[i][1].append(line[i])
-
-    # Find the maximum length of each column
-    for i, (_, v) in enumerate(columns):
-        columns[i][0] = len(max([w.strip() for w in v], key=len))
-    lines[1] = ["-" * (l + 1) for l, _ in columns]
-
-    # Join the lines together into a list of formatted strings
-    for i, line in enumerate(lines):
-        for j, v in enumerate(line):
-            line[j] = v.strip().ljust(columns[j][0] + 2)
-        lines[i] = "".join(lines[i])
-    lines[1] = "-" * (
-        sum(columns[i][0] for i, _ in enumerate(columns)) + 2 * (len(columns) - 1)
-    )
-    return lines
-
-
 def digital_watch(hours: int, minutes: int) -> list[str]:
     guard_inputs(hours, minutes)
     minutes_tens, minutes_ones = str(minutes).rjust(2, "0")
@@ -413,7 +355,7 @@ def print_ascii(ascii_array: list[str]) -> None:
 
 
 def ncurses_ascii(ascii_array: list[str]) -> None:
-    from curses import wrapper, use_default_colors
+    from curses import use_default_colors, wrapper
 
     def inner(stdscr):
         use_default_colors()
@@ -421,25 +363,21 @@ def ncurses_ascii(ascii_array: list[str]) -> None:
         for row in ascii_array:
             stdscr.addstr(row + "\n")
         stdscr.refresh()
-        return stdscr.getkey()
+        stdscr.getkey()
 
     wrapper(inner)
 
 
 def main(args: str = "", xtime: tuple[int, int] = (0, 0)) -> None:
     if "h" in args:
-        import os
-
         print(
             "\n".join(
                 md_table_to_lines(
-                    19, 28, f"{os.path.dirname(__file__)}/README.md", ["**"]
+                    19, 28, f"{os.path.dirname(__file__)}/README.md", ("**",)
                 )
             )
         )
         return
-
-    from datetime import datetime
 
     time = (
         xtime
@@ -455,8 +393,6 @@ def main(args: str = "", xtime: tuple[int, int] = (0, 0)) -> None:
 
 
 if __name__ == "__main__":
-    import sys
-
     if len(sys.argv) > 3 and sys.argv[1].startswith("-"):
         main(sys.argv[1][1:], (int(sys.argv[2]), int(sys.argv[3])))
     elif len(sys.argv) > 1 and sys.argv[1].startswith("-"):
